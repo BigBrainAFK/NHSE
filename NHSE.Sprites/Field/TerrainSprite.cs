3dddfbc9bdc02be4f6604909a27b21cbacbb2984
BigBrainAFK <tony.langhammer@yahoo.de>
2020-04-11 11:18:09 +0200

    Implement tiling

--- NHSE.Sprites/Field/TerrainSprite.cs
+++ NHSE.Sprites/Field/TerrainSprite.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using NHSE.Core;
+using NHSE.Sprites.Properties;
 
 namespace NHSE.Sprites
 {
@@ -37,6 +38,149 @@ namespace NHSE.Sprites
             if (num < 0)
                 return name;
             return name.Substring(0, num) + Environment.NewLine + name.Substring(num);
+        }
+
+        public static Image GetTileImage(TerrainTile tile)
+        {
+            var tileName = tile.UnitModel.ToString();
+            var tileRotation = tile.TileRotation;
+            var pathSpriteID = tile.PathSpriteID.ToString();
+            var pathSpriteRotation = tile.PathSpriteRotation;
+
+            if (pathSpriteID != "" && pathSpriteID != "Base") tileName = pathSpriteID;
+
+            if (pathSpriteRotation == 0 && tileRotation > 0) pathSpriteRotation = tileRotation;
+
+            Image baseSprite = (Image)Resources.ResourceManager.GetObject("Base");
+
+            if (tileName.Substring(0, 4) == "Fall")
+                baseSprite = (Image)Resources.ResourceManager.GetObject(tileName);
+
+            if (baseSprite == null) baseSprite = (Image)Resources.ResourceManager.GetObject("unknown");
+
+            if (baseSprite == null) throw new KeyNotFoundException("Resources not found in ResourceManager");
+
+            // baseSprite rotation 1 = 90degree left
+
+            if (tileRotation > 0)
+            {
+                baseSprite.RotateFlip((RotateFlipType)((4 - tileRotation) % 4));
+            }
+
+            if (tileName != "Base")
+                return OverlayImages(baseSprite, GetPathSprite(tileName, pathSpriteRotation));
+
+            return baseSprite;
+        }
+
+        private static Image GetPathSprite(string spriteID = "", ushort rotation = 0)
+        {
+            if (spriteID != "")
+            {
+                var spritePattern = spriteID.Substring(spriteID.Length - 2);
+                var spriteMaterial = spriteID.Substring(0, spriteID.Length - 2);
+                var spriteMod = "";
+
+                if (spriteMaterial == "Cliff")
+                    spriteMod = "_C";
+
+                if (spriteMaterial == "River")
+                    spriteMod = "_R";
+
+                if (rotation < 0 || rotation > 3) rotation = 0;
+
+                Image tileSprite = (Image)Resources.ResourceManager.GetObject(spritePattern + spriteMod);
+
+                if (tileSprite == null) tileSprite = (Image)Resources.ResourceManager.GetObject(spritePattern);
+
+                if (tileSprite == null)
+                {
+                    tileSprite = (Image)Resources.ResourceManager.GetObject("unknown");
+                }
+                else
+                {
+                    tileSprite.RotateFlip((RotateFlipType)((4 - rotation) % 4));
+                }
+
+                return ApplySpriteMaterial(tileSprite, spriteMaterial);
+            }
+            else
+            {
+                Image tileSprite = (Image)Resources.ResourceManager.GetObject("unknown");
+
+                return tileSprite;
+            }
+        }
+
+        public static Image ApplySpriteMaterial(Image sprite, string material)
+        {
+            Image materialImage = (Image)Resources.ResourceManager.GetObject(material);
+
+            if (materialImage == null) return sprite;
+
+            // Create Bitmaps and lock them
+            Bitmap materialBitmap = new Bitmap(materialImage);
+
+            Rectangle materialBitmapRect = new Rectangle(0, 0, materialBitmap.Width, materialBitmap.Height);
+            System.Drawing.Imaging.BitmapData materialBitmapData =
+                materialBitmap.LockBits(materialBitmapRect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                materialBitmap.PixelFormat);
+
+            Bitmap imageMask = new Bitmap(sprite);
+
+            Rectangle imageMaskRect = new Rectangle(0, 0, imageMask.Width, imageMask.Height);
+            System.Drawing.Imaging.BitmapData imageMaskData =
+                imageMask.LockBits(imageMaskRect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                imageMask.PixelFormat);
+
+            // Get pointers to first lines
+            IntPtr materialBitmapLinePtr = materialBitmapData.Scan0;
+            IntPtr imageMaskLinePtr = imageMaskData.Scan0;
+
+            // Create arrays for all bytes of the bitmap
+            int materialBitmapBytes = Math.Abs(materialBitmapData.Stride) * materialBitmap.Height;
+            byte[] materialBitmapRGBA = new byte[materialBitmapBytes];
+            int imageMaskBytes = Math.Abs(imageMaskData.Stride) * imageMask.Height;
+            byte[] imageMaskRGBA = new byte[imageMaskBytes];
+
+            // Copy all the RGBA value from the Bitmaps to the arrays
+            System.Runtime.InteropServices.Marshal.Copy(materialBitmapLinePtr, materialBitmapRGBA, 0, materialBitmapBytes);
+            System.Runtime.InteropServices.Marshal.Copy(imageMaskLinePtr, imageMaskRGBA, 0, imageMaskBytes);
+
+            // Copy colors of all pixels that are not transparent  
+            for (int y = 3; y < imageMaskRGBA.Length; y += 4)
+            {
+                if (imageMaskRGBA[y] == 0) continue;
+                imageMaskRGBA[y - 3] = materialBitmapRGBA[y - 3];
+                imageMaskRGBA[y - 2] = materialBitmapRGBA[y - 2];
+                imageMaskRGBA[y - 1] = materialBitmapRGBA[y - 1];
+                imageMaskRGBA[y] = materialBitmapRGBA[y];
+            }
+
+            // Copy the RGBA value back into the Bitmaps
+            System.Runtime.InteropServices.Marshal.Copy(materialBitmapRGBA, 0, materialBitmapLinePtr, materialBitmapBytes);
+            System.Runtime.InteropServices.Marshal.Copy(imageMaskRGBA, 0, imageMaskLinePtr, imageMaskBytes);
+
+            // Lock the Bitmaps
+            materialBitmap.UnlockBits(materialBitmapData);
+            imageMask.UnlockBits(imageMaskData);
+
+            return (Image)imageMask;
+        }
+
+        // https://stackoverflow.com/questions/38566828/overlap-one-image-as-transparent-on-another-in-c-sharp
+        public static Image OverlayImages(Image background, Image foreground)
+        {
+            if (background.Height != foreground.Height || background.Width != foreground.Width) throw new ArgumentException("Both images need to be the same size");
+            Bitmap resultImage = new Bitmap(background);
+            Graphics tempCompose = Graphics.FromImage(resultImage);
+
+            tempCompose.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceOver;
+
+            tempCompose.DrawImage(background, 0, 0);
+            tempCompose.DrawImage(foreground, 0, 0);
+
+            return resultImage;
         }
 
         public static Bitmap CreateMap(TerrainManager mgr)
---
+++ NHSE.Sprites/SpriteBitmap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+
+namespace NHSE.Sprites
+{
+    class SpriteBitmap
+    {
+        public SpriteBitmap(Image image)
+        {
+            Bitmap materialBitmap = new Bitmap(image);
+
+            Rectangle materialBitmapRect = new Rectangle(0, 0, materialBitmap.Width, materialBitmap.Height);
+            System.Drawing.Imaging.BitmapData materialBitmapData =
+                materialBitmap.LockBits(materialBitmapRect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                materialBitmap.PixelFormat);
+
+            IntPtr materialBitmapLinePtr = materialBitmapData.Scan0;
+
+            int materialBitmapBytes = Math.Abs(materialBitmapData.Stride) * materialBitmap.Height;
+            byte[] materialBitmapRGBA = new byte[materialBitmapBytes];
+
+            System.Runtime.InteropServices.Marshal.Copy(materialBitmapLinePtr, materialBitmapRGBA, 0, materialBitmapBytes);
+
+            System.Runtime.InteropServices.Marshal.Copy(materialBitmapRGBA, 0, materialBitmapLinePtr, materialBitmapBytes);
+
+            materialBitmap.UnlockBits(materialBitmapData);
+        }
+    }
+}
